611efa67d59aff881182cb19223753fb1bf0c79f
DevS-PMA <Sharma.diwakar2009@gmail.com>
2025-09-07 22:35:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Backend.journal_router import journal_router
 app = FastAPI ()
 
 app.include_router(auth_router, prefix="/auth")
-app.include_router(chat_router, prefix="/chat")
+app.include_router(chat_router)
 app.include_router(journal_router, prefix="/journal")
 
 app.add_middleware(
